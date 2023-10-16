65cc91285098594843ce8d62a0476948e4c886e9
koseus <mehmetkse@gmail.com>
2023-10-16 23:24:28 +0300

    First commit

---
+++ resources/js/Pages/Index/Display.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        Display
+    </div>
+    <Link href="/">Main Page</Link>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/inertia-vue3'
+</script>
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Index
+    </div>
+    <Link href="/hello">Display Page</Link>
+
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/inertia-vue3'
+</script>
