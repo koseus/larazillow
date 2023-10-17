f3106e8f90900db356042a0a05624ce3fdf239ab
koseus <mehmetkse@gmail.com>
2023-10-17 10:14:28 +0300

    Add persistent layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,20 +1,24 @@
 <template>
-    <MainLayout>
-        <div>
-            Index
-        </div>
-        <Link href="/hello">Display Page</Link>
-        <div>
-            The message is {{ message }}
-        </div>
-    </MainLayout>
+    <div>
+        Index
+    </div>
+    <Link href="/hello">Display Page</Link>
+    <div>
+        The message is {{ message }}
+    </div>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/inertia-vue3'
-    import MainLayout from '../../Layouts/MainLayout.vue'
     defineProps({
         message: String
     })
 
+</script>
+
+<script>
+    import MainLayout from '../../Layouts/MainLayout.vue';
+    export default {
+        layout: MainLayout
+    }
 </script>
