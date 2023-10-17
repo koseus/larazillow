689b685dbf2efd9986fd964d79c005d6f38cdbb0
koseus <mehmetkse@gmail.com>
2023-10-17 09:50:07 +0300

    Add layouts

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <Link href="/">Main Page</Link>
+    <Link href="/hello">Display Page</Link>
+    <slot>Default</slot>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/inertia-vue3'
+</script>
--- resources/js/Pages/Index/Display.vue
+++ resources/js/Pages/Index/Display.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-        Display
-    </div>
-    <Link href="/">Main Page</Link>
+    <MainLayout>
+        <div>
+            Display
+        </div>
+        <Link href="/">Main Page</Link>
+        </MainLayout>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/inertia-vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue';
 </script>
