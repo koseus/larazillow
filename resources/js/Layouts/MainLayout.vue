275cee526730300bd6fa681e8d4734f9e7b7781c
koseus <mehmetkse@gmail.com>
2023-10-19 10:55:04 +0300

    Update listing page design with css

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div>The page with time {{ timer }}</div> -->
 
-  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
@@ -24,12 +24,11 @@
 
   <main class="container mx-auto p-4 ">
     <div
-      v-if="true"
+      v-if="flashSuccess"
       class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800
       bg-green-50 dark:bg-green-900 p-2"
     >
-      <!-- {{ flashSuccess }} -->
-      I'm a flash!
+      {{ flashSuccess }}
     </div>
     <slot>Default</slot>
   </main>
