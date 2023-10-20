51404747402016eb23b5e89e3d484eb56590fe4b
koseus <mehmetkse@gmail.com>
2023-10-20 22:05:04 +0300

    Add registration form

---
+++ resources/js/Pages/UserAccount/Create.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="register">
+    <div class="w-1/2 mx-auto">
+      <div>
+        <label for="name" class="label">
+          Your Name
+        </label>
+        <input id="name" v-model="form.name" class="input" type="text" />
+        <div v-if="form.errors.name" class="input-error">{{ form.errors.name }}</div>
+      </div>
+
+      <div class="mt-4">
+        <label for="email" class="label">
+          E-mail (username)
+        </label>
+        <input id="email" v-model="form.email" class="input" type="text" />
+        <div v-if="form.errors.email" class="input-error">{{ form.errors.email }}</div>
+      </div>
+
+      <div class="mt-4">
+        <label for="password" class="label">
+          Password
+        </label>
+        <input id="password" v-model="form.password" class="input" type="password" />
+        <div v-if="form.errors.password" class="input-error">{{ form.errors.password }}</div>
+      </div>
+
+      <div class="mt-4">
+        <label for="password_confirmation" class="label">
+          Confirm Password
+        </label>
+        <input id="password_confirmation" v-model="form.password_confirmation" class="input" type="password" />
+      </div>
+
+      <div class="mt-4">
+        <button class="btn-primary w-full" type="submit">
+          Create Account
+        </button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3'
+
+const form = useForm({
+  name: null,
+  email: null,
+  password: null,
+  password_confirmation: null,
+
+})
+
+const register = () => form.post(route(''))
+</script>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 
@@ -33,5 +34,6 @@ Route::post('login', [AuthController::class, 'store'])
 Route::delete('logout', [AuthController::class, 'destroy'])
     ->name('logout');
 
-
+Route::resource('user-account', UserAccountController::class)
+    ->only(['create']);
 ?>
