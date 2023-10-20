5db51588cad8059743d40e643fbdd2f6748ade8b
koseus <mehmetkse@gmail.com>
2023-10-20 10:31:34 +0300

    Add AuthController and routes for it

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
@@ -20,4 +21,10 @@ Route::get('/hello', [IndexController::class, 'display']);
 
 Route::resource('listing', ListingController::class);
 
+Route::get('login', [AuthController::class, 'create'])
+    ->name('login');
+Route::post('login', [AuthController::class, 'store'])
+    ->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+    ->name('logout');
 ?>
