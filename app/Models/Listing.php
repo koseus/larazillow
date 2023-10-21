24123633c90cd3620a76ddb268c10390a76b0a6b
koseus <mehmetkse@gmail.com>
2023-10-21 23:28:54 +0300

    Add local query scopes in Eloquent

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Listing extends Model
 {
@@ -20,5 +22,10 @@ class Listing extends Model
             User::class,
             'by_user_id'
         );
+    }
+
+    public function scopeMostRecent(Builder $query): Builder
+    {
+        return $query->orderByDesc('created_at');
     }
 }
