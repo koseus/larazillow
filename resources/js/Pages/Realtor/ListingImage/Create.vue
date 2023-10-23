d935ffeb6e163b6c45155bdc7cb8ba23de7c55b2
koseus <mehmetkse@gmail.com>
2023-10-23 22:03:58 +0300

    Add multiple file uploading for realtor listing - FE

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,15 +1,42 @@
 <template>
-  <form
-    enctype="multipart/form-data"
-    method="post" :action="route('realtor.listing.image.store', { listing: listing.id })"
-  >
-    <input type="file" multiple name="images[]" />
-    <button type="submit">Send</button>
-  </form>
+  <Box>
+    <template #header>
+      Upload new images
+    </template>
+    <form @submit.prevent="upload">
+      <input type="file" multiple @input="addFiles" />
+      <button type="submit">Upload</button>
+      <button type="reset" @click="reset">Reset</button>
+    </form>
+  </Box>
 </template>
 
 <script setup>
-defineProps({
+import Box from '@/Components/UI/Box.vue'
+import { useForm } from '@inertiajs/inertia-vue3'
+
+const props = defineProps({
   listing: Object,
 })
+
+const form = useForm({
+  images: [],
+})
+
+const upload = () => {
+  form.post(
+    route('realtor.listing.image.store', { listing: props.listing.id}),
+    {
+      onSuccess: () => form.reset('images'),
+    },
+  )
+}
+
+const addFiles = (event) => {
+  for(const image of event.target.files){
+    form.images.push(image)
+  }
+}
+
+const reset = () => form.reset('images')
 </script>
